c00edf848499ed6dbc6b45eb62d31ca0b2b34080
Amanda Hargrove <amandahargrove98@gmail.com>
2022-06-29 21:49:46 -0400

    wrote some js pseudocode

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
-window.addEventListener("DOMContentLoaded", function () {
-  // Execute after page load
-});
+// window.addEventListener("DOMContentLoaded", function () {
+//   // Execute after page load
+// });
+
+/*
+ PSEUDO CODE/NOTES:
+
+ - player 1 is dealt 2 cards up at the start
+ if (player clicks deal button) {
+    show 2 cards in their deck
+ }
+ - then, after that, the dealer is given 2 cards and one of them is facing down
+ if (player clicks deal button) {
+  dealer shows 2 cards, one up and one with the back showing
+ }
+ - When the player clicks hit, the player is given another card next to their other cards
+ element.onclick = add another card to their deck
+
+ - When the player clicks stay, the dealer gets their turn and flips up their card. If the dealer's cards equal 16 or less, they have to add a card to the deck. If their two cards equal 17 or less, they have to stay
+
+ - If the player's cards add up to over 21, then it is a bust and it's game over
+
+
+
+
+*/
